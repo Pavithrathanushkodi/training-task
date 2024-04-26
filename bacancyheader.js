e96e0c16e5fd4cff5c09c1e42a3c20606abda499
Pavithrathanushkodi <158037361+Pavithrathanushkodi@users.noreply.github.com>
2024-04-26 23:08:41 +0530

    Add files via upload

---
+++ bacancyheader.js
@@ -0,0 +1,38 @@
+// var darktheme;
+// function changeHeaderColor(){
+
+//     darktheme=!darktheme;
+
+//    if(selectvalue==white){
+//     document.documentElement.style.setProperty("--bgcolor1", "white");
+//     document.documentElement.style.setProperty("--bgcolor2", "black");
+//    }
+//     else{
+//         document.documentElement.style.setProperty("--bgcolor1", "red");
+//     document.documentElement.style.setProperty("--bgcolor2", "black");
+//     }
+// }
+
+        
+
+// code for select the option value for change the header color
+
+// function myfunction(col){
+//     if(col==blue){
+//     document.getElementById("header").style.backgroundColor=blue;
+//     }
+//     else if(col==red){
+//         document.getElementById("header").style.backgroundColor=red;
+//     }
+//     else if(col==violet){
+//         document.getElementById("header").style.backgroundColor=violet;
+//     }
+// }
+
+
+
+function myfunction(color){cd
+    document.documentElement.style.setProperty("--bgcolor1", color);
+    
+    document.documentElement.style.setProperty("--bgcolor2", "black");
+}
